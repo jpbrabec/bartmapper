392098a10f0d1eb9f23839d63275b53a1b685f3f
Jake Brabec <jbrabbers@gmail.com>
2018-10-14 19:26:46 -0700

    First pass on serviceworker improvements

---
+++ public/serviceWorker.js
@@ -0,0 +1,70 @@
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  'index.html',
+  './', // Alias for index.html
+  'styles.css',
+  '../../styles/main.css',
+  'demo.js'
+];
+
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  console.log('install event');
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener('activate', event => {
+  console.log('activate event');
+
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+
+  // Do not cache the route lookup because it changes each time
+  let shouldCache = !event.request.url.includes(`sched.aspx`);
+  console.log("Should cache " + event.request.url + "? "+shouldCache);
+  if(shouldCache) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          console.log('Cache hit!!')
+          return cachedResponse;
+        }
+        console.log('aww cache miss :(')
+
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            console.log('cahcin away')
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+
+});
--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
-serviceWorker.register();
+
+//Register service worker
+if('serviceWorker' in navigator) {
+  //Wait until the page has loaded
+  window.addEventListener('load', () => {
+    var serviceWorkerUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`
+    console.log(`Registering service worker ${serviceWorkerUrl}`);
+    navigator.serviceWorker.register(serviceWorkerUrl, {scope: '/'})
+      .then(reg => {
+        console.log('Registration succeeded. Scope is ' + reg.scope)
+      })
+      .catch(e => {
+      console.log('Error sad' + e)
+    });
+  });
+} else {
+  console.log('Navigator does not support serviceWorker')
+}
